052c1cdc50978ac3955898b061b0aade24bf39dd
liaozhongkai <LZK1997714>
2020-08-29 14:21:00 +0800

    feat: 添加协程

--- app/src/main/java/com/lzk/jetpacktest/coroutine/CoroutineActivity.kt
+++ app/src/main/java/com/lzk/jetpacktest/coroutine/CoroutineActivity.kt
@@ -7,6 +7,7 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import com.lzk.jetpacktest.R
 import kotlinx.android.synthetic.main.activity_coroutine.*
+import kotlinx.coroutines.delay
 import java.util.*
 
 class CoroutineActivity : AppCompatActivity() {
